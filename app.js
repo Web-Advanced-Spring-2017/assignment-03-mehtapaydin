b29616c20c0438dad1a045d8124edb1d38b2a43e
mehtapaydin <mehtapaydinnnn@gmail.com>
2017-04-21 12:57:37 -0400

    Changed the socket structure

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-
-// Express requires these dependencies
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
   , path = require('path');
 
-var app = express()
+var app = express();
 
-var logger = require('morgan')
-var bodyParser = require('body-parser')
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000)
 app.set('views', __dirname + '/views')
@@ -17,25 +15,24 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.get('/', routes.index);
 
-// app.use(notFoundHandler)
-// app.use(errorHandler)
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-// function notFoundHandler (req, res, next) {
-//     var err = new Error('Not Found')
-//     err.status = 404
-//     next(err)
-// }
+function notFoundHandler (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err)
+}
 
-// function errorHandler (err, req, res, next) {
-//     res.locals.message = err.message
-//     res.locals.error = req.app.get('env') === 'development' ? err : {}
-//     res.status(err.status || 500)
-//     res.render('error')
-// }
+function errorHandler (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+    res.status(err.status || 500);
+    res.render('error')
+}
 
 // Enable Socket.io
 var server = http.Server(app);
@@ -44,42 +41,31 @@ var io = require('socket.io')(server);
 // A user connects to the server (opens a socket)
 io.on('connection', function (socket) {
 
-  console.log("a user connected: " + socket.id);
+    console.log("a user connected: " + socket.id);
 
-  socket.on('disconnect', function(){
-      console.log('user disconnected');
-  });
+    socket.on('disconnect', function () {
+        console.log('user disconnected: ' + socket.id);
+    });
 
-  socket.emit('welcome', { hello: 'world' });
-  socket.on('thanks', function (data) {
-      console.log(data);
-  });
+    // A User starts a path
+    socket.on('server:startPath', function (data, sessionId) {
+        console.log('server get startPath from: ' + sessionId);
+        socket.broadcast.emit('client:startPath', data, sessionId);
+    });
 
-  // A User starts a path
-  socket.on( 'startPath', function( data, sessionId ) {
+    // A User continues a path
+    socket.on('server:continuePath', function (data, sessionId) {
+        console.log('server get continuePath from: ' + sessionId);
+        socket.broadcast.emit('client:continuePath', data, sessionId);
+    });
 
-      console.log('sessionId');
-    socket.broadcast.emit( 'startPath', data, sessionId );
-
-  });
-
-  // A User continues a path
-  socket.on( 'continuePath', function( data, sessionId ) {
-
-    socket.broadcast.emit( 'continuePath', data, sessionId );
-
-  });
-
-  // A user ends a path
-  socket.on( 'endPath', function( data, sessionId ) {
-
-    socket.broadcast.emit( 'endPath', data, sessionId );
-
-  });  
+    // A user ends a path
+    socket.on('server:endPath', function (data, sessionId) {
+        console.log('server get endPath from: ' + sessionId);
+        socket.broadcast.emit('client:endPath', data, sessionId);
+    });
 
 });
-
-exports.io = io
 
 server.listen( app.get('port'), function() {
     console.log('Server listening at port: ' + app.get('port'))
--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -1,167 +1,83 @@
-// -----------
-// Setup
-// -----------
+// each user/client/browser opens a socket and sessionId is its identifier.
+var socket = io();
+var sessionId = '';
+paths = {};
 
+socket.on('connect', function() {
 
-// The faster the user moves their mouse
-// the larger the circle will be
-// We dont want it to be larger/smaller than this
-tool.maxDistance = 2;
-tool.maxDistance = 80;
+  // when client connected to the server, set its id as sessionId
+  sessionId = socket.id;
 
-// Each user has a unique session ID
-// We'll use this to keep track of paths
-var sessionId = io.socket.sessionid;
+  // put session id to the screen
+  var text = new PointText(new Point(10, 10));
+  text.fillColor = 'black';
+  text.content = 'My identifier (sessionId): ' + sessionId;
+  view.draw();
 
-// Returns an object specifying a semi-random color
+});
+
+socket.on( 'client:startPath', function( data, sessionId ) {
+  startPath(data.point, data.color, sessionId);
+});
+
+socket.on( 'client:continuePath', function( data, sessionId ) {
+  continuePath(data.top, data.bottom, sessionId);
+  view.draw();
+});
+
+socket.on( 'client:endPath', function( data, sessionId ) {
+  endPath(data.point, sessionId);
+  view.draw();
+});
+
 function randomColor() {
-  
   return {
     hue: Math.random() * 360,
     saturation: 0.8,
     brightness: 0.8,
     alpha: 0.5
   };
-
 }
 
-// An object to keep track of each users paths
-// We'll use session ID's as keys
-paths = {};
-
-
-
-
-
-// -----------
-// User Events
-// -----------
-
-
-// The user started a path
 function onMouseDown(event) {
-  
-  // Create the new path
   color = randomColor();
-
-  startPath( event.point, color, sessionId );
-
-  // Inform the backend
-  emit("startPath", {point: event.point, color: color}, sessionId);
-
+  startPath(event.point, color, sessionId);
+  socket.emit('server:startPath', {point: event.point, color: color}, sessionId);
 }
 
 function onMouseDrag(event) {
 
-  var step        = event.delta / 2;
-  step.angle     += 90; 
-  var top         = event.middlePoint + step;
-  var bottom      = event.middlePoint - step;
+  var step = event.delta / 2;
+  step.angle += 90;
+  var top = event.middlePoint + step;
+  var bottom = event.middlePoint - step;
 
-  continuePath( top, bottom, sessionId );
-
-  // Inform the backend
-  emit("continuePath", {top: top, bottom: bottom}, sessionId);
-
+  continuePath(top, bottom, sessionId);
+  socket.emit('server:continuePath', {top: top, bottom: bottom}, sessionId);
 }
 
 function onMouseUp(event) {
-
   endPath(event.point, sessionId);
-
-  // Inform the backend
-  emit("endPath", {point: event.point}, sessionId);
-
+  socket.emit('server:endPath', {point: event.point}, sessionId);
 }
 
-
-
-
-
-
-// -----------------
-// Drawing functions
-// Use to draw multiple users paths
-// -----------------
-
-
 function startPath( point, color, sessionId ) {
-
   paths[sessionId] = new Path();
   paths[sessionId].fillColor = color;
   paths[sessionId].add(point);
-
 }
 
 function continuePath(top, bottom, sessionId) {
-
-  var path    = paths[sessionId];
-  
+  var path = paths[sessionId];
   path.add(top);
   path.insert(0, bottom);
-
 }
 
 function endPath(point, sessionId) {
-
   var path = paths[sessionId];
-
   path.add(point);
   path.closed = true;
   path.smooth();
 
   delete paths[sessionId]
-
 }
-
-
-
-
-
-
-// -----------------
-// Emit
-// Use to inform the server of user events
-// -----------------
-
-
-function emit(eventName, data) {
-
-  io.emit(eventName, data, sessionId);
-
-}
-
-
-
-
-
-
-
-// -----------------
-// On
-// Draw other users paths
-// -----------------
-
-
-
-io.on( 'startPath', function( data, sessionId ) {
-  
-  startPath(data.point, data.color, sessionId);
-  
-})
-
-
-io.on( 'continuePath', function( data, sessionId ) {
-
-  continuePath(data.top, data.bottom, sessionId);
-  view.draw();
-  
-})
-
-
-io.on( 'endPath', function( data, sessionId ) {
-
-  endPath(data.point, sessionId);
-  view.draw();
-  
-})
